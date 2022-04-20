6f8192daf9dc0cfd03469187673c0f2e741ebead
aabidkhan786 <abidkhan786@outlook.com>
2022-04-20 13:34:32 +0530

    Typescript Initial Commit

---
+++ Variable.js
@@ -0,0 +1,14 @@
+// In Javascript Style
+var personName = 'Alexander';
+console.log(personName);
+var int_num = 5;
+console.log(int_num);
+var hasSummer = true;
+console.log(hasSummer);
+// In TypeScript Style
+var name1 = 'Benjamin';
+console.log(name1);
+var int_num1 = 25;
+console.log(int_num1);
+var isSummer = true;
+console.log(isSummer);
---
+++ Variable.ts
@@ -0,0 +1,18 @@
+// In Javascript Style
+let personName = 'Alexander';
+//It will create a typescript error but it compile out as we haven't mention the type of the variables.
+console.log(personName);
+let int_num = 5;
+console.log(int_num);
+let hasSummer = true;
+console.log(hasSummer);
+
+
+
+// In TypeScript Style
+let name1: string = 'Benjamin';
+console.log(name1);
+let int_num1: number = 25;
+console.log(int_num1);
+let isSummer: boolean = true;
+console.log(isSummer);
