650aa62eeab95303af31f1991178226641c14e5d
aabidkhan786 <abidkhan786@outlook.com>
2022-04-20 13:37:46 +0530

    Array type in Typescript and JS

---
+++ Array.js
@@ -0,0 +1,6 @@
+// Declaring Array in Typescript of String Types
+var names = [];
+names.push("Benjamin");
+names.push("Susan M. Gorman");
+names.push("Stephanie L. McVicker");
+console.log(names);
---
+++ Array.ts
@@ -0,0 +1,8 @@
+// Declaring Array in Typescript of String Types
+
+const names: string[] = [];
+names.push("Benjamin")
+names.push("Susan M. Gorman")
+names.push("Stephanie L. McVicker")
+
+console.log(names);
