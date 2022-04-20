513f94973c0e7fbd52c674875d4c9fb6c9395215
aabidkhan786 <abidkhan786@outlook.com>
2022-04-20 13:45:45 +0530

    readonly method applied

--- Array.js
+++ Array.js
@@ -4,3 +4,9 @@ names.push("Benjamin");
 names.push("Susan M. Gorman");
 names.push("Stephanie L. McVicker");
 console.log(names);
+// Readonly keyword prevent array from being changed
+var name_stack1 = ["Jesus P. Dutra", "Camille V. Miles", "Bill J. Newton", "Annette A. White"];
+//trying to add name in the array
+// name_stack1.push("Lisa D. Hanson")
+//The push method above will create an error as we use readonly method, so we can't push a string into it.
+console.log(name_stack1);
--- Array.ts
+++ Array.ts
@@ -6,3 +6,12 @@ names.push("Susan M. Gorman")
 names.push("Stephanie L. McVicker")
 
 console.log(names);
+
+// Readonly keyword prevent array from being changed
+const name_stack1: readonly string[] = ["Jesus P. Dutra", "Camille V. Miles", "Bill J. Newton", "Annette A. White"]
+
+//trying to add name in the array
+// name_stack1.push("Lisa D. Hanson")
+//The push method above will create an error as we use readonly method, so we can't push a string into it.
+
+console.log(name_stack1)
