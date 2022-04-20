2704eb54c985bbf5427ecd151d7ab07a492f3a1d
aabidkhan786 <abidkhan786@outlook.com>
2022-04-20 15:10:48 +0530

    Array Types in TS

--- Array.js
+++ Array.js
@@ -10,3 +10,23 @@ var name_stack1 = ["Jesus P. Dutra", "Camille V. Miles", "Bill J. Newton", "Anne
 // name_stack1.push("Lisa D. Hanson")
 //The push method above will create an error as we use readonly method, so we can't push a string into it.
 console.log(name_stack1);
+//Declaring Array in Typescript of Number Types
+var ids = [1, 2, 3, 4, 5];
+console.log(ids);
+// But if i try to add a string value into the ids array then:
+// ids.push("Peter Decker")
+// It will generate an error : Argument of type 'string' is not assignable to parameter of type 'number'.
+console.log(ids);
+// Similarly we have the boolean type array
+var options = [true, true, false, true, false];
+console.log(options);
+// Array of object types would be like: 
+var books = [
+    { name: "JavaScript: The Good Parts", author: "Douglas Crockford" },
+    { name: "Effective JavaScript: 68 Specific Ways to Harness the Power of Javascript", author: "David Herman" }
+];
+console.log(books);
+// And now we have the array of any types
+var arrStack = ["Peter Decker", 5025745514, true];
+arrStack.push("PeterDecker@teleworm.us");
+console.log(arrStack);
--- Array.ts
+++ Array.ts
@@ -15,3 +15,30 @@ const name_stack1: readonly string[] = ["Jesus P. Dutra", "Camille V. Miles", "B
 //The push method above will create an error as we use readonly method, so we can't push a string into it.
 
 console.log(name_stack1)
+
+//Declaring Array in Typescript of Number Types
+let ids: number[] = [1, 2, 3, 4, 5];
+console.log(ids);
+
+// But if i try to add a string value into the ids array then:
+// ids.push("Peter Decker")
+// It will generate an error : Argument of type 'string' is not assignable to parameter of type 'number'.
+console.log(ids);
+
+// Similarly we have the boolean type array
+let options: boolean[] = [true, true, false, true, false]
+console.log(options);
+
+// Array of object types would be like: 
+let books: object[] = [
+    { name: "JavaScript: The Good Parts", author: "Douglas Crockford" },
+    { name: "Effective JavaScript: 68 Specific Ways to Harness the Power of Javascript", author: "David Herman" }
+]
+
+console.log(books);
+
+
+// And now we have the array of any types
+let arrStack = ["Peter Decker", 5025745514, true]
+arrStack.push("PeterDecker@teleworm.us")
+console.log(arrStack);
