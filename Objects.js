a180e1bf88a8f467d9225b08eea63e772cfb93e1
aabidkhan786 <abidkhan786@outlook.com>
2022-04-21 13:42:45 +0530

    Method declaration inside object TS

--- Objects.js
+++ Objects.js
@@ -11,13 +11,15 @@ var Ayyub = {
     full_name: 'Ayyub Ruwaid Nader',
     phone_number: 88827121,
     email: "AyyubRuwaidNader@armyspy.com",
-    isProgrammer: true
+    isProgrammer: true,
+    sayHi: function (name) { return "Hello, Mr. ".concat(name); }
 };
-console.log(Ayyub);
+console.log(Ayyub.sayHi(Ayyub.full_name));
 var Long_Shih = {
     full_name: 'Long Shih',
     phone_number: 6077344686,
     email: "LongShih@armyspy.com",
-    isProgrammer: true
+    isProgrammer: true,
+    sayHi: function (name) { return "Hello, Mr. ".concat(name); }
 };
 console.log(Long_Shih);
--- Objects.ts
+++ Objects.ts
@@ -21,22 +21,25 @@ interface PersonDetails {
     phone_number: number;
     email: string;
     isProgrammer: boolean;
+    sayHi: (name: string) => string;
 }
 
 let Ayyub: PersonDetails = {
     full_name: 'Ayyub Ruwaid Nader',
     phone_number: 88827121,
     email: "AyyubRuwaidNader@armyspy.com",
-    isProgrammer: true
+    isProgrammer: true,
+    sayHi: (name: string) => `Hello, Mr. ${name}`
 }
 
-console.log(Ayyub);
+console.log(Ayyub.sayHi(Ayyub.full_name));
 
 let Long_Shih: PersonDetails = {
     full_name: 'Long Shih',
     phone_number: 6077344686,
     email: "LongShih@armyspy.com",
-    isProgrammer: true
+    isProgrammer: true,
+    sayHi: (name: string) => `Hello, Mr. ${name}`
 }
 
 console.log(Long_Shih);
