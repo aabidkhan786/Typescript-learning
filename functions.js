eed5493caca5e5f301d5524c8900f93c36da0849
aabidkhan786 <abidkhan786@outlook.com>
2022-04-21 13:44:41 +0530

    Fixed decimal added

--- functions.js
+++ functions.js
@@ -3,3 +3,8 @@ function circle(diam) {
     return 'The circumference is ' + (Math.PI * diam).toFixed(2);
 }
 console.log(circle(10));
+// Define a function called perimeter that takes a side variable of type number and return a string but this time using the arrow function
+var perimeter = function (side) {
+    return 'The perimeter is : ' + (4 * side).toFixed(2);
+};
+console.log(perimeter(2.5));
--- functions.ts
+++ functions.ts
@@ -4,3 +4,10 @@ function circle(diam: number): string {
 }
 
 console.log(circle(10));
+
+// Define a function called perimeter that takes a side variable of type number and return a string but this time using the arrow function
+const perimeter = (side: number): string => {
+    return 'The perimeter is : ' + (4 * side).toFixed(2);
+}
+
+console.log(perimeter(2.5));
