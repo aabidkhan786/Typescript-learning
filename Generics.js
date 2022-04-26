b7451ac27008debf1b6e7f01d491d06a9b24e178
aabidkhan786 <abidkhan786@outlook.com>
2022-04-26 11:56:23 +0530

    Generics Types Added

--- Generics.js
+++ Generics.js
@@ -10,3 +10,16 @@ console.log(PrintData(typeof 25)); // Output will be number
 console.log(PrintData(typeof "25")); // String
 console.log(PrintData(typeof false)); // boolean
 // As we have the above data type of any so our argument can strings, numbers, booleans, array, objects.So now to change the type of arguments in the above functions.
+function print_data(data) {
+    return data;
+}
+console.log(print_data({ name: "Ruby Walker", age: 29, place: "Germany" }));
+// As you can see we have declared the functions to object type, argument to be object type and the return value to be object type.
+console.log(print_data("Ruby Walker"));
+console.log(typeof (print_data("Ruby Walker")));
+console.log(print_data(typeof ("Ruby Walker")));
+// Now we have the functions to be string type, argument to be string types and return value to be of string types.
+console.log(print_data(625));
+console.log(typeof (print_data(625)));
+console.log(print_data(typeof (625)));
+// Now we have the functions to be numbers type, argument to be numbers types and return value to be of numbers types.
--- Generics.ts
+++ Generics.ts
@@ -14,3 +14,20 @@ console.log(PrintData(typeof "25"));        // String
 console.log(PrintData(typeof false));       // boolean
 
 // As we have the above data type of any so our argument can strings, numbers, booleans, array, objects.So now to change the type of arguments in the above functions.
+
+function print_data<T>(data: T): T {
+    return data
+}
+console.log(print_data<object>({ name: "Ruby Walker", age: 29, place: "Germany" }));
+// As you can see we have declared the functions to object type, argument to be object type and the return value to be object type.
+
+console.log(print_data<string>("Ruby Walker"));
+console.log(typeof (print_data<string>("Ruby Walker")));
+console.log(print_data(typeof (<string>("Ruby Walker"))));
+// Now we have the functions to be string type, argument to be string types and return value to be of string types.
+
+console.log(print_data<number>(625));
+console.log(typeof (print_data<number>(625)));
+console.log(print_data(typeof (<number>(625))));
+// Now we have the functions to be numbers type, argument to be numbers types and return value to be of numbers types.
+
